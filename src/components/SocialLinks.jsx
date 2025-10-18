876a0ae1787edd97949967f02d2a204bc547619e
sntdashi <sntdashi@gmail.com>
2025-10-18 03:53:29 +0000

    update

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -15,7 +15,7 @@ const socialLinks = [
     displayName: "Let's Connect",
     subText: "on LinkedIn",
     icon: Linkedin,
-    url: "https://www.linkedin.com/in/rawrzn/",
+    url: "https://www.linkedin.com/in/rauzzan-muhamad-fikri/",
     color: "#0A66C2",
     gradient: "from-[#0A66C2] to-[#0077B5]",
     isPrimary: true,
